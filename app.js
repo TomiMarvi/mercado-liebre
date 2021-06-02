a6873fe7f6a20a0ce05cec44903148ff7ec311a5
TomiMarvi <tomas.martinezvila@gmail.com>
2021-06-02 19:05:07 -0300

    toques finales

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname,"./public")));
+
+app.set("port", process.env.PORT || 3000);
+
+app.listen(app.get("port"), ()=>{console.log("Server start in: http://localhost:" + app.get("port"))});
+
+app.get("/", (req, res)=>{res.sendFile(path.join(__dirname,"views", "home.html"))});
+
+app.get("/login", (req, res)=>{res.sendFile(path.join(__dirname,"views", "login.html"))});
+
+app.get("/register", (req, res)=>{res.sendFile(path.join(__dirname,"views", "register.html"))});
